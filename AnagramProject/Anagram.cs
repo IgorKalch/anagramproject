49431a63610296ab494c71ae4abd4b84e9b5fb49
HART\i.kalchenko <i.kalchenko@hartphp.com.pl>
2022-10-23 22:37:43 +0200

    Create Project. Create class Anagram. Add to class 2 methods: ReverseSingleWord() and ReverseString().

---
+++ AnagramProject/Anagram.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AnagramProject
+{
+    public class Anagram
+    {
+        string ReverseSingleWord(string data)
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            char[] charArray = data.ToCharArray();
+            int lenFromBeginOfString = data.Length - 1;
+            int lenFromEndOfString = 0;
+
+            while (lenFromEndOfString < lenFromBeginOfString)
+            {
+                // Ignore special characters
+                if (!char.IsLetter(charArray[lenFromEndOfString]))
+                    lenFromEndOfString++;
+                else if (!char.IsLetter(charArray[lenFromBeginOfString]))
+                    lenFromBeginOfString--;
+
+                // Both charArray[lenFromEndOfString] and charArray[lenFromBeginOfString]) are not spacial
+                else
+                {
+                    char tmp = charArray[lenFromEndOfString];
+                    charArray[lenFromEndOfString] = charArray[lenFromBeginOfString];
+                    charArray[lenFromBeginOfString] = tmp;
+                    lenFromEndOfString++;
+                    lenFromBeginOfString--;
+                }
+            }
+            return new string(charArray);
+        }
+
+        public string ReverseString(string data)
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+            string[] subs = Regex.Split(data, @"(?<=[\s])");
+            string concatRevsrsedString = "";
+
+            foreach (string sub in subs)
+            {
+                concatRevsrsedString += ReverseSingleWord(sub);
+            }
+            return concatRevsrsedString;
+        }
+
+
+    }
+}
---
+++ AnagramProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToReverse = "A!b Cde#f  ewrff    jk";
+
+            Anagram anagram = new Anagram();
+
+            Console.WriteLine(anagram.ReverseString(wordToReverse));
+
+            Console.ReadLine();
+        }
+    }
+}
