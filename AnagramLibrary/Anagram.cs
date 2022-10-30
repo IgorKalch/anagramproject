06836919b6d937c4dca6daf1caa3248fe1f8b6a0
HART\i.kalchenko <i.kalchenko@hartphp.com.pl>
2022-10-30 12:03:51 +0100

    Deletion the method: ReverseSingleWord. Create new test methods

--- AnagramLibrary/Anagram.cs
+++ AnagramLibrary/Anagram.cs
@@ -1,43 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace AnagramLibrary
 {
     public class Anagram
     {
-        public string ReverseSingleWord(string data)
-        {
-            if (string.IsNullOrEmpty(data))
-                return data;
-
-            char[] charArray = data.ToCharArray();
-            int lenFromEndOfString = data.Length - 1;
-            int lenFromBeginOfString = 0;
-
-            while (lenFromBeginOfString < lenFromEndOfString)
-            {
-                if (!char.IsLetter(charArray[lenFromBeginOfString]))
-                    lenFromBeginOfString++;
-
-                else if (!char.IsLetter(charArray[lenFromEndOfString]))
-                    lenFromEndOfString--;
-
-                else
-                {
-                    char tmp = charArray[lenFromBeginOfString];
-                    charArray[lenFromBeginOfString] = charArray[lenFromEndOfString];
-                    charArray[lenFromEndOfString] = tmp;
-                    lenFromBeginOfString++;
-                    lenFromEndOfString--;
-                }
-            }
-            return new string(charArray);
-        }
-
         public string ReverseString(string data)
         {
             if (string.IsNullOrEmpty(data))
@@ -48,11 +14,30 @@ namespace AnagramLibrary
 
             foreach (string sub in subs)
             {
-                concatRevsrsedString += ReverseSingleWord(sub);
+                int lenFromEndOfString = sub.Length - 1;
+                int lenFromBeginOfString = 0;
+                char[] charArray = sub.ToCharArray();
+
+                while (lenFromBeginOfString < lenFromEndOfString)
+                {
+                    if (!char.IsLetter(charArray[lenFromBeginOfString]))
+                        lenFromBeginOfString++;
+
+                    else if (!char.IsLetter(charArray[lenFromEndOfString]))
+                        lenFromEndOfString--;
+
+                    else
+                    {
+                        char tmp = charArray[lenFromBeginOfString];
+                        charArray[lenFromBeginOfString] = charArray[lenFromEndOfString];
+                        charArray[lenFromEndOfString] = tmp;
+                        lenFromBeginOfString++;
+                        lenFromEndOfString--;
+                    }
+                }
+                concatRevsrsedString += new string(charArray);
             }
             return concatRevsrsedString;
         }
-
-
     }
 }
--- AnagramTest/AnagramTest.cs
+++ AnagramTest/AnagramTest.cs
@@ -7,38 +7,60 @@ namespace AnagramTest
     [TestClass]
     public class AnagramTest
     {
+        Anagram anagram = new Anagram();
+
         [TestMethod]
-        public void ReverseSingleWordTest()
+        [DataRow("abc", "cba")]
+        public void ReverseStringTest_Reverse_AllSymbolsWillPermut(string initial, string expected)
         {
-            //Arrange 
-            Anagram anagram = new Anagram();
-
-            var data = "Abc";
-            var reverceData = "cbA";
-
-            //Act
-            var result = anagram.ReverseSingleWord(data);
-
-            //Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(reverceData, result);
-            
+            var result = anagram.ReverseString(initial);
+            Assert.AreEqual(result, expected);
         }
 
         [TestMethod]
-        public void ReverseStringTest()
+        [DataRow("1234", "1234")]
+        public void ReverseStringTest_Reverse_AllSymbolsStayOnTheSamePosition(string initial, string expected)
         {
-            //Arrange 
-            Anagram anagram = new Anagram();
-            var data = "A#bc  dfff!$ f#df";
-            var reverceData = "c#bA  fffd!$ f#df";
+            var result = anagram.ReverseString(initial);
+            Assert.AreEqual(result, expected);
+        }
 
-            //Act
-            var result = anagram.ReverseString(data);
+        [TestMethod]
+        [DataRow("abc!+!abc", "cba!+!cba")]
+        public void ReverseStringTest_Reverse_WordWithMixedSymbols(string initial, string expected)
+        {
+            var result = anagram.ReverseString(initial);
+            Assert.AreEqual(result, expected);
+        }
 
-            //Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(reverceData,result);
+        [TestMethod]
+        [DataRow("\t\tabc\t\tabc\t\t", "\t\tcba\t\tcba\t\t")]
+        [DataRow(" abc\nabc\t\t", " cba\ncba\t\t")]
+        [DataRow(" abc\nabc\t\t", " cba\ncba\t\t")]
+        public void ReverseStringTest_Reverse_WhitespacesKeepTheSameCountAndPosition(string initial, string expected)
+        {
+            var result = anagram.ReverseString(initial);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        [DataRow("", "")]
+        [DataRow(null, "")]
+        public void ReverseStringTest_Reverse_NullOrEmptyTextAreAccepted(string initial, string expected)
+        {
+            var result = anagram.ReverseString(initial);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        [DataRow("abcd efgh", "dcba hgfe")]
+        [DataRow("Test", "tseT")]
+        [DataRow("a1bcd efg!h", "d1cba hgf!e")]
+        [DataRow(" a1bcd efg!h", " d1cba hgf!e")]
+        public void ReverseStringTest_QuickTest(string initial, string expected)
+        {
+            var result = anagram.ReverseString(initial);
+            Assert.AreEqual(result, expected);
         }
     }
 }
